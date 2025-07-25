d9c04084fd2f01ad458581044ade989a902994c4
Emi <157651749+Emi8977@users.noreply.github.com>
2025-07-25 20:14:24 -0300

    Update login.js

--- frontend/static/js/login.js
+++ frontend/static/js/login.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     try {
-      const res = await fetch("http://localhost:5002/login", {
+      const res = await fetch("/api/login", { //cambiamos la ruta de /localhost por /api
         method: "POST",
         headers: {
           "Content-Type": "application/json"
