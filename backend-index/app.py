5700efe19994c3f7dc59ea7699a336d7d197021c
Emi <157651749+Emi8977@users.noreply.github.com>
2025-08-22 15:20:15 -0300

    Update app.py [deploy-dev]

--- backend-index/app.py
+++ backend-index/app.py
@@ -35,7 +35,7 @@ mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-    return jsonify({"status": "ok", "message": "hola..."})
+    return render_template('index.html')
 
 
 @app.route('/login')
@@ -59,6 +59,7 @@ def dashboard():
 
 if __name__ == '__main__':
     app.run(port=5000)
+
 
 
 
